e706cc97957db7ce32b3e69f5a999f176965f747
Vivernum_ <pandatv22rus@gmail.com>
2025-09-22 14:04:07 +0700

    модуль говна

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -2,13 +2,35 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 
+const compare = createComparison(defaultRules); 
+
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+
+    Object.keys(indexes)
+        .forEach((elementName) => {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName])
+                .map(name => {
+                    const newElement = document.createElement('option');
+                    newElement.value = name;
+                    newElement.textContent = name;
+                    return newElement;
+                })
+            )
+        });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
 
+        if (!action) return;
+        else if (action === 'clear') {
+            const parent = action.parentElement;
+            const target = parent.querySelector('input');
+            target.value = '';
+        }
+
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state)); 
     }
 }
--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import {initTable} from "./components/table.js";
 
 import {initPagination} from "./components/pagination.js";
 import {initSorting} from './components/sorting.js';
+import {initFiltering} from './components/filtering.js';
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -43,6 +44,7 @@ function render(action) {
 
     result = applyPagination(result, state, action);
     result = applySorting(result, state, action);
+    result = applyFiltering(result, state, action);
 
     sampleTable.render(result)
 }
@@ -50,7 +52,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header'],
+    before: ['header', 'filter'],
     after: ['pagination']
 }, render);
 
@@ -72,6 +74,10 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
 ]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+})
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
